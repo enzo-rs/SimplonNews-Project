62b3b585bc9acc73f072a0358ef37d2d340d57c4
enzo-rs <enzorojoall@gmail.com>
2021-02-11 17:07:05 +0100

    Fix utils js

--- static/js/login.js
+++ static/js/login.js
@@ -1,12 +1,5 @@
 /* Signin redirection start */
-function changePage() {
-    let link = document.querySelector('div.form-links > a:not(.active)');
-
-    link.addEventListener('click', () => {
-        document.location.href = "../../index.html"
-    })
-}
-changePage()
+changePage('../../index.html')
 
 /* Signin redirection end */
 
@@ -36,69 +29,37 @@ function accessUser(email, password) {
 
             if (response.status == 400) {
                 alertBox(`Email ou mot de passe incorrect.`);
-                let cross = document.querySelectorAll('.errors-container .fa-times');
-
-                cross.forEach((e) => {
-                    e.addEventListener('click', () => {
-                        e.parentElement.parentElement.remove()
-                    })
-                })
             } else
                 response.json()
                 .then(function (data) {
-                     let token = data.token;
+                    let token = data.token;
 
-                     sessionStorage.setItem('token', token);
+                    sessionStorage.setItem('token', token);
                     document.location.href = '../views/home.html'
 
                 })
-                
+
         })
 }
 
-function validateForm() {
-
-    if (passwordInput.value == null || passwordInput.value == "") {
-        return false;
-    } else if (emailInput.value == null || emailInput.value == "") {
-        return false;
-    } else {
-        return true
-    }
-}
 
 
-function alertBox(string) {
-    let container = document.querySelector('main .errors-container');
-    let error = document.createElement('div');
-    error.classList.add('errors');
-    error.innerHTML = `<p>${string}<p>
-    <i class="fas fa-times"></i>`;
-    container.appendChild(error)
-}
+
 
 formElement.addEventListener('submit', (e) => {
     e.preventDefault();
     let emailValue = emailInput.value;
     let passwordValue = passwordInput.value;
 
+    let validate = validateForm([emailInput, passwordInput])
 
-    let cross = document.querySelectorAll('.errors-container .fa-times');
-
-    cross.forEach((e) => {
-        e.addEventListener('click', () => {
-            e.parentElement.parentElement.remove()
-        })
-    })
-
-
-    if (validateForm()) {
+    if (validate) {
         accessUser(emailValue, passwordValue);
         emailInput.value = "";
         passwordInput.value = "";
         document.querySelector('.errors-container').textContent = '';
     } else {
-
+        alertBox('Veuillez remplir tous les champs')
     }
 })
 
